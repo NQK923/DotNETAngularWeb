9389c5dde804b8cd88afefa2d5c0c6cc4717bf9e
NGUYỄN QUÝ KHÁNH <117274353+NQK923@users.noreply.github.com>
2024-10-25 22:16:29 +0700

    Delete FrontEnd directory

--- FrontEnd/frontend.client/src/app/Model/ModelNotification.ts
+++
@@ -1,7 +0,0 @@
-export interface ModelNotification {
-    id_Notification: number; // Ensure camelCase
-    content: string; // Ensure lowercase
-    time: Date; 
-    isRead: boolean;
-    type_Noti: string;
-}
